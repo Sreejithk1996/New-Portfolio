7a9db15de64e0aa1bcd8ec070fc64039dab3df48
Sreejithk1996 <sreekumar1996@gmail.com>
2024-06-04 17:50:25 +0530

    updated

--- src/components/my-projects/MyProjects.jsx
+++ src/components/my-projects/MyProjects.jsx
@@ -1,35 +1,37 @@
-import React from 'react'
-import "../../styles/MyProjects.scss"
-import {projects} from "../../utilities/projects"
-import open from "../../assets/images/export.png"
+import React from 'react';
+import "../../styles/MyProjects.scss";
+import { projects } from "../../utilities/projects";
+import open from "../../assets/images/export.png";
 
 function MyProjects() {
   return (
-    <div className='my-projects'>
-        <div className='title-3'>
-            <p>My <label>Projects</label></p>
-        </div>
-        <div className='project-list'>
-            {
-                projects.map((list)=>{
-                    return(
-                        <div className={list.class} key={list.id}>
-                            <div className='project-section-1'>
-                                <img src={list.img} alt="" />
-                            </div>
-                            <div className='project-section-2'>
-                                <p className='project-id'>{list.num}</p>
-                                <p className='project-title'>{list.title}</p>
-                                <p className='project-details'>{list.details}</p>
-                                <a href="https://metrictreelabs.com/" target='blank'><img src={open} alt="" /></a>
-                            </div>
-                        </div>
-                    )
-                })
-            }
-        </div>      
+    <div className="my-projects">
+      <div className="title-3">
+        <p>My <label>Projects</label></p>
+      </div>
+      <div className="project-list">
+        {projects.map((list) => (
+          <div
+            className={list.class}
+            style={{ flexDirection: list.id % 2 === 0 ? "row-reverse" : "row" }}
+            key={list.id}
+          >
+            <div className="project-section-1">
+              <img src={list.img} alt="" />
+            </div>
+            <div className="project-section-2">
+              <p className="project-id">{list.num}</p>
+              <p className="project-title">{list.title}</p>
+              <p className="project-details">{list.details}</p>
+              <a href={list.path} target="_blank">
+                <img src={open} alt="" />
+              </a>
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
-  )
+  );
 }
 
-export default MyProjects
+export default MyProjects;
--- src/utilities/projects.js
+++ src/utilities/projects.js
@@ -9,16 +9,18 @@ const projects = [
         class: "project",
         title: "Metric Tree Labs Landing Page",
         details: "Designed the landing page for Metric Tree Labs, a software development company, using HTML, CSS, Tailwind CSS, and React. Crafted a user-friendly and visually appealing experience that effectively highlights Metric Tree Labs' services and expertise. The focus was on creating a responsive design that adapts flawlessly to any device, showcasing a clear value proposition for potential clients.", 
-        img: mtl
+        img: mtl,
+        path: "https://github.com/Sreejithk1996/Metric-Tree-Labs"
     },
 
     {
-        id: 1,
+        id: 2,
         num: "02",
         class: "project",
         title: "League Boat Race",
-        details: "Designed the landing page for Metric Tree Labs, a software development company, using HTML, CSS, Tailwind CSS, and React. Crafted a user-friendly and visually appealing experience that effectively highlights Metric Tree Labs' services and expertise. The focus was on creating a responsive design that adapts flawlessly to any device, showcasing a clear value proposition for potential clients.", 
-        img: boat
+        details: "Designed and developed a landing page for the Rotary Club of Cochin South. This project utilized HTML, CSS, and Bootstrap to create a user-friendly and informative platform. The focus was on responsiveness, ensuring the page functions flawlessly across devices.  The landing page clearly outlines the Rotary Club's mission, activities, and membership benefits, making it a valuable tool for attracting new members and promoting community engagement.", 
+        img: boat,
+        path: "https://github.com/Sreejithk1996/League-Boat-Race"
     }
 ]   
 
