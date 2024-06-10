b4451a6c709f26be71fae0bd2967c71a3c142642
Sreejithk1996 <sreekumar1996@gmail.com>
2024-06-10 16:49:38 +0530

    updated

--- src/components/scroll-top/ScrollTop.jsx
+++ src/components/scroll-top/ScrollTop.jsx
@@ -1,37 +1,3 @@
-// import React, {useEffect,useState} from 'react'
-// import arrowup from "../../assets/images/arrow-up.svg"
-// import "../../styles/ScrollTop.scss"
-
-// const ScrollTop = () => {
-//     const [showScrollTop,setShowScrollTop] = useState(false);
-
-//     useEffect(() => {
-//         window.addEventListener("scroll", () => {
-//             if(window.scrollY > 300) {
-//                 setShowScrollTop(true)
-//             } else {
-//                 setShowScrollTop(false)
-//             }
-//         })
-//     },[]);
-
-//     const scrollTop = () => {
-//         window.scrollTo({
-//             top:0,
-//             behavior: 'smooth',
-//         });
-//     };
-//   return (
-//     <div className='scroll-top' onClick={scrollTop}>
-//         {showScrollTop && (
-//         <img src={arrowup} alt="" />)}
-//     </div>
-//   )
-// }
-
-// export default ScrollTop
-
-
 import React, { useEffect, useState } from 'react';
 import arrowup from "../../assets/images/arrow-up.svg";
 import "../../styles/ScrollTop.scss";
